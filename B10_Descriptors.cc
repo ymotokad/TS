9c7127f82348102ecf2c2b1506bd9e920bd2d354
yoshi <>
2017-10-08 14:51:15 +0000

    *** empty log message ***

--- B10_Descriptors.cc
+++ B10_Descriptors.cc
@@ -14,6 +14,7 @@
   You should have received a copy of the GNU General Public License
   along with TS software suite.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 static char rcsid[] = "@(#)$Id$";
 #ifdef __GNUG__
 #pragma implementation
